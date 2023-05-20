08d9122f3cd3921e1c5876f13356725315475650
nary <narylozano@gmail.com>
2023-05-20 17:56:48 +0200

    Add global styles and theme

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Red Hat Display', sans-serif;
+  font-weight: 400;
+}
+
+body {
+  background-color: #fff;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+p {
+  margin: 0;
+}
+
+button {
+  border: none;
+  font: inherit;
+  cursor: pointer;
+}
+
+input {
+  font: inherit;
+  border: none;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+ul
+{
+list-style: none;
+padding: 0;
+margin: 0;
+}
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    light: "#fff",
+    dark: "#000",
+  },
+
+  fonts: {
+    primary: "Red Hat Display, sans-serif",
+  },
+
+  fontSizes: {
+    medium: "1rem",
+  },
+
+  spacing: {
+    medium: "2rem",
+  },
+};
+
+export default theme;
