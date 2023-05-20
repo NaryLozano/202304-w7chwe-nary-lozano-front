5e351e0c2c3d45859a85545233d5564b24b30383
nary <narylozano@gmail.com>
2023-05-20 19:23:06 +0200

    Add loginPage

--- src/routers/loginRouter.tsx
+++ src/routers/loginRouter.tsx
@@ -1,13 +1,19 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, RouteObject, Navigate } from "react-router-dom";
 import App from "../components/App/App";
-import LoginForm from "../components/LoginForm/LoginForm";
+import LoginFormPage from "../pages/loginPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to={"/login"} replace />,
+      },
+    ],
   },
-  { path: "/login", element: <LoginForm /> },
+  { path: "/login", element: <LoginFormPage /> },
 ];
 
 const appRouter = createBrowserRouter(routes);
