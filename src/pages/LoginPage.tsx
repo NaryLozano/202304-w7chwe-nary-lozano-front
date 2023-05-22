5f7df3e1509ca1317cbdd93942459e182dd534e4
nary <narylozano@gmail.com>
2023-05-22 13:11:32 +0200

    Add token hook to decode token and update login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,23 @@
 import LoginForm from "../components/LoginForm/LoginForm.js";
 import useUser from "../hooks/useUser.js";
+import { useAppDispatch } from "../store/index.js";
+import useToken from "../store/token/useToken.js";
+import { loginUserActionCreator } from "../store/users/usersSlice.js";
 import UserCredentials from "../types.js";
 import LoginPageStyled from "./LoginPageStyled.js";
 
 const LoginFormPage = (): JSX.Element => {
   const { getUserToken } = useUser();
+  const { getTokenData } = useToken();
+  const dispatch = useAppDispatch();
 
   const handleFormSubmit = async (user: UserCredentials) => {
     const token = await getUserToken(user);
     localStorage.setItem("token", token);
+
+    const userData = getTokenData(token);
+
+    dispatch(loginUserActionCreator(userData));
   };
 
   return (
