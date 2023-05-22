67c8d60128b75029b433c6ca366bf3d72761b759
nary <narylozano@gmail.com>
2023-05-22 11:18:10 +0200

    Add function to save in localStorage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,19 @@
 import LoginForm from "../components/LoginForm/LoginForm.js";
+import useUser from "../hooks/useUser.js";
+import UserCredentials from "../types.js";
 import LoginPageStyled from "./LoginPageStyled.js";
 
 const LoginFormPage = (): JSX.Element => {
+  const { getUserToken } = useUser();
+
+  const handleFormSubmit = async (user: UserCredentials) => {
+    const token = await getUserToken(user);
+    localStorage.setItem("token", token);
+  };
+
   return (
     <LoginPageStyled>
-      <LoginForm></LoginForm>{" "}
+      <LoginForm loginSubmitForm={handleFormSubmit} />
     </LoginPageStyled>
   );
 };
